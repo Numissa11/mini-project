c82cdd6e64b5b49ccb5863a8df940b400ac90bad
Aline <aline_sinclaire@hotmail.fr>
2020-09-08 22:05:48 +0200

    react starter pack Api fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from './components/user';
+import axios from 'axios';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      users: []
+    }
+  }
+
+  handleUser = () => {
+
+    let url = `https://jsonplaceholder.typicode.com/users`
+    axios.get(url)
+      .then(response => this.setState({ users: response }))
+     .then(response => console.log(this.state.users))
+  }
+
+  render() {
+    return (
+
+      <div>
+        <User />
+        <Button variant="contained" color="secondary" size="small" onClick={this.handleUser}>Search</Button>
+      </div>
+    );
+  }
 }
-
 export default App;
