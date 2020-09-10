7f11ee5a1e0c664f72eab07aa46824113779e7cb
Aline <aline_sinclaire@hotmail.fr>
2020-09-10 22:07:48 +0200

    user is mapped to the front separately with the delet button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
       users: []
     }
   }
@@ -18,23 +17,47 @@ class App extends React.Component {
 
     let url = `https://jsonplaceholder.typicode.com/users`
     axios.get(url)
-      .then(response => this.setState({ users: response.data }, () => console.log('Fresh data fetched..', response)))
-      .then(() => console.log(this.state.users))
+    .then(response => this.setState({ users: response.data }, () => console.log('Fresh data fetched in State..', this.state)))
   }
+/*
+
+  deleteCharacter = (id) => {
+    const users = this.state.users.filter(user => user.id !== id)
+    this.setState({ users })
+  }*/
+
+  // TODO: When adding a new user think about how can i copy the state property (users) as I cannot mutate the state directly e.g. this.state.users.push(users)
+
+  // Spread operator || create a copy
+  
 
   render() {
     return (
 
+
+      
       <div>
 
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}> Name: {user.name} Username: {user.high} email: {user.email} Adresse: {user.adresse}</li>
-          )}
-        </ul>
+        {this.state.users.map((user) => (
+            <User
+              {...user}
+              key={user.id}
+            />
+          ))}
 
-        <User />
+        <ul>
+          {/* TODO: When mapping each user, how can I create a reusable component and pass data as props to this component (User Component) */}
+          {/* TODO: How can I implement the delete button on every single component */}
+          {/*this.state.users.map(user =>
+            <li key={user.id}> Name: {user.name} --- Username: {user.username} --- email: {user.email} --- City: 
+            {user['address']['city']} --- Latitude: {user['address']['geo']['lat']} 
+            --- Longitude: {user['address']['geo']['lng']}</li>
+          )*/}
+        </ul>
         <Button variant="contained" color="secondary" size="small" onClick={this.handleUser}>Search</Button>
+
+        
+        
       </div>
     );
   }
--- src/components/user.js
+++ src/components/user.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
-function User() {
-      return (
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            name: this.props.name,
+            username: this.props.username,
+            email: this.props.email,
+            id: this.props.id
+        }
+    }
+    /*
+
+    handleChange = (event) => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.editCharacter(this.state)
+        this.setState({ editMode: false });
+    }
+
+
+*/
+    render() {
+        console.log(this.state)
+        const { name, username, email, id } = this.state
+        return (
             <div>
-                  <h1>User</h1>
+
+                <p className="card-title">{name}</p>
+                <p className="card-title">{username}</p>
+                <p className="card-text">{email}</p>
+                <p className="card-text">{id}</p>
+
+
+                <Button variant="contained" color="secondary" size="small" onClick={() => this.props.deleteCharacter(id)}>Delete</Button>
+
+
             </div>
-      )
+        )
+    }
 }
 
 export default User;
